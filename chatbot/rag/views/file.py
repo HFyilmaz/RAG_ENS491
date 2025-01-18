2340f3ce7996efa3d2f21e208a68ba86890acf2a
Anil <anilsen@sabanciuniv.edu>
2025-01-18 23:51:30 +0300

    minor update

--- chatbot/rag/views/file.py
+++ chatbot/rag/views/file.py
@@ -10,6 +10,7 @@ import os
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 
+
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
