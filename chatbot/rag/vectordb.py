f3e17e53b84882af4b6bbdd5eea84dbede8d05b8
Anil <a.sen-1@student.utwente.nl>
2025-01-18 23:37:21 +0300

    elastic update for api models

--- chatbot/rag/vectordb.py
+++ chatbot/rag/vectordb.py
@@ -58,7 +58,7 @@ def add_to_chroma(chunks: list[Document]):
 
     # Load the existing database.
     db = Chroma(
-        persist_directory=CHROMA_PATH, embedding_function=get_embedding_function_ollama()
+        persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
     )
 
     # Calculate Page IDs.
@@ -140,19 +140,19 @@ def get_embedding_function():
 #chunks = split_documents(documents)
 #print(chunks[0])
 
-from langchain_ollama import OllamaEmbeddings
+#from langchain_ollama import OllamaEmbeddings
 
-
+'''
 def get_embedding_function_ollama():
     embeddings = OllamaEmbeddings(model="nomic-embed-text")
     return embeddings
-
+'''
 def delete_file_from_chroma(filename):
     """Delete all chunks for a given filename from Chroma database"""
     try:
         db = Chroma(
             persist_directory=CHROMA_PATH,
-            embedding_function=get_embedding_function_ollama()
+            embedding_function=get_embedding_function()
         )
         # Get all document IDs that start with the file URL
         file_url = f"http://127.0.0.1:8000/media/rag_database/{filename}"
