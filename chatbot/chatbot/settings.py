84a8138b7e1062c845028026149ff06bc9301dfe
Anil <anilsen@sabanciuniv.edu>
2025-02-03 13:09:26 +0300

    issue fixed. "http://localhost" had to be included in CORS allowed origins list.

--- chatbot/chatbot/settings.py
+++ chatbot/chatbot/settings.py
@@ -162,7 +162,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # React development server
     "http://127.0.0.1:5173",
     "http://frontend:80",
-    "http://localhost:80"
+    "http://localhost:80",
+    "http://localhost"
 
 ]
 
